0735c9e7e97949d6caa3597d962e224dc3e67bd0
tiagoCarb <tiagocarbonelli@gmail.com>
2025-07-25 22:00:51 -0300

    Add the logic to play a single round
    add function playRound to play a round

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 function getComputerChoice() {
     let number = Math.floor(Math.random() * 100)
-    if (number<=33) {
-        return "rock"
-    } else if (number<=66 && number>33) {
-        return "paper"
+    if (number <= 33) {
+        return "rock";
+    } else if (number <= 66 && number > 33) {
+        return "paper";
     } else {
-        return "scissors"
+        return "scissors";
     }
 }
 
 function getHumanChoice() {
-let human = prompt("Rock, paper, scissors?", "")
-return human
+    let human = prompt("Rock, paper, scissors?", "");
+    return human.toLowerCase();
 }
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice = "scissors") && (computerChoice = "paper")) {
+        console.log("You Win! Scissors beats Paper");
+        return ++humanChoice
+    } else if ((humanChoice = "paper") && (computerChoice = "rock")) {
+        console.log("You Win! Paper beats Rock");
+        return ++humanChoice
+    } else if ((humanChoice = "rock") && (computerChoice = "scissors")) {
+        console.log("You Win! Rock beats Scissors");
+        return ++humanChoice
+    } else if ((humanChoice = "paper") && (computerChoice = "scissors")) {
+        console.log("You Lose! Scissors beats Paper")
+        return ++computerChoice
+    } else if ((humanChoice = "rock") && (computerChoice = "paper")) {
+        console.log("You Lose! Paper beats Rock");
+        return ++computerChoice
+    } else if ((humanChoice = "scissors") && (computerChoice = "rock")) {
+        console.log("You Lose! Rock beats Scissors");
+        return ++computerChoice
+    } else {
+        console.log("It's a Draw!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
